e9a62fa6db0ece44239e878cac1c128f5fa3aa1f
Habibur Rahaman <shamolrahaman@gmail.com>
2025-11-09 21:05:38 +0600

    Add pageTitle helper to BaseController

--- src/Base/Http/Controllers/BaseController.php
+++ src/Base/Http/Controllers/BaseController.php
@@ -3,7 +3,18 @@
 namespace Botble\Base\Http\Controllers;
 
 use Illuminate\Routing\Controller as LaravelController;
+use Illuminate\Support\Facades\View;
 
 abstract class BaseController extends LaravelController
 {
+    protected function pageTitle(string $title, ?string $subTitle = null): static
+    {
+        View::share('pageTitle', $title);
+
+        if ($subTitle !== null) {
+            View::share('pageSubTitle', $subTitle);
+        }
+
+        return $this;
+    }
 }
